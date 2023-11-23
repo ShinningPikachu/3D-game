6711986ef0f7cff134bba32b3375d47306380328
jinheng.lin <jinheng.lin@estudiantat.upc.edu>
2023-11-23 15:08:27 +0100

    all

---
+++ 3D-Game/Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public float rotationSpeed, jumpSpeed, gravity;
+
+    Vector3 startDirection;
+    float speedY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Store starting direction of the player with respect to the axis of the level
+        startDirection = transform.position - transform.parent.position;
+        startDirection.y = 0.0f;
+        startDirection.Normalize();
+
+        speedY = 0;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        CharacterController charControl = GetComponent<CharacterController>();
+        Vector3 position;
+
+        // Left-right movement
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            float angle;
+            Vector3 direction, target;
+
+            position = transform.position;
+            angle = rotationSpeed * Time.deltaTime;
+            direction = position - transform.parent.position;
+            if (Input.GetKey(KeyCode.A))
+            {
+                target = transform.parent.position + Quaternion.AngleAxis(angle, Vector3.up) * direction;
+                if (charControl.Move(target - position) != CollisionFlags.None)
+                {
+                    transform.position = position;
+                    Physics.SyncTransforms();
+                }
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                target = transform.parent.position + Quaternion.AngleAxis(-angle, Vector3.up) * direction;
+                if (charControl.Move(target - position) != CollisionFlags.None)
+                {
+                    transform.position = position;
+                    Physics.SyncTransforms();
+                }
+            }
+        }
+
+        // Correct orientation of player
+        // Compute current direction
+        Vector3 currentDirection = transform.position - transform.parent.position;
+        currentDirection.y = 0.0f;
+        currentDirection.Normalize();
+        // Change orientation of player accordingly
+        Quaternion orientation;
+        if ((startDirection - currentDirection).magnitude < 1e-3)
+            orientation = Quaternion.AngleAxis(0.0f, Vector3.up);
+        else if ((startDirection + currentDirection).magnitude < 1e-3)
+            orientation = Quaternion.AngleAxis(180.0f, Vector3.up);
+        else
+            orientation = Quaternion.FromToRotation(startDirection, currentDirection);
+        transform.rotation = orientation;
+
+        // Apply up-down movement
+        position = transform.position;
+        if (charControl.Move(speedY * Time.deltaTime * Vector3.up) != CollisionFlags.None)
+        {
+            transform.position = position;
+            Physics.SyncTransforms();
+        }
+        if (charControl.isGrounded)
+        {
+            if (speedY < 0.0f)
+                speedY = 0.0f;
+            if (Input.GetKey(KeyCode.W))
+                speedY = jumpSpeed;
+        }
+        else
+            speedY -= gravity * Time.deltaTime;
+    }
+}
+
+
