4b05729ab3e8f309ef43fc0b50b7c1357b840c95
jinheng.lin <jinheng.lin@estudiantat.upc.edu>
2023-12-07 03:49:21 +0100

    plataform mov

--- 3D-Game/Assets/Scripts/Plataform.cs
+++ 3D-Game/Assets/Scripts/Plataform.cs
@@ -7,30 +7,51 @@ public class Plataform : MonoBehaviour
     // Start is called before the first frame update
     public int state;// 0 idel 1 openning 2 closing
     Vector3 startDirection;
+    bool opencheck;
+    bool closecheck;
+
+    float radi;
     void Start()
-    {
+    {   
+        radi = Mathf.Sqrt(transform.position.x*transform.position.x + transform.position.z*transform.position.z);
         // Store starting direction of the player with respect to the axis of the level
         startDirection = transform.position - transform.parent.position;
         startDirection.y = 0.0f;
         startDirection.Normalize();
         state = 0;
-
+        opencheck = false;
+        closecheck = false;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Moving();
-        /*if(Input.GetKey(KeyCode.O)){
+        if(Input.GetKey(KeyCode.O) && !opencheck){
             open();
+            opencheck = true;
+            /*Moving();
+            Vector3 t = Vector3.zero;
+            t.y = transform.position.y;
+            transform.position = t;*/
         }
-        else if(Input.GetKey(KeyCode.P)){
+        else if(Input.GetKeyUp(KeyCode.O) && opencheck){
+            opencheck = false;
+        }
+        if(Input.GetKey(KeyCode.P) && !closecheck){
             close();
-        }*/
+            closecheck = true;
+        }else if(Input.GetKeyUp(KeyCode.O) && closecheck){
+            closecheck = false;
+        }
+
+        
+
         if(state == 1){
             openning();
+            state = 0;
         }else if(state == 2){
             closing();
+            state = 0;
         }
     }
 
@@ -46,51 +67,35 @@ public class Plataform : MonoBehaviour
 
     void openning()
     {
-        float angle = Mathf.Atan2(transform.position.x + transform.parent.position.x, transform.position.z + transform.parent.position.z);
-        Vector3 pos = transform.position + transform.parent.position;
-        pos.x = (float)4*Mathf.Sin(angle);
-        pos.z = (float)4*Mathf.Cos(angle);
-        transform.position = pos - transform.parent.position;
+        
+        float angle = Mathf.Atan2(transform.position.x, transform.position.z);
+        Vector3 pos = transform.position;
+        pos.x = (float)radi*Mathf.Sin(angle)*-15.0f;
+        pos.z = (float)radi*Mathf.Cos(angle)*-15.0f;
+        transform.position = pos;
     }
 
     void closing()
     {
-        float angle = Mathf.Atan2(transform.position.x + transform.parent.position.x, transform.position.z + transform.parent.position.z);
-        Vector3 pos = transform.position + transform.parent.position;
-        pos.x = (float)2.91*Mathf.Sin(angle);
-        pos.z = (float)2.91*Mathf.Cos(angle);
-        transform.position = pos - transform.parent.position;
+        float angle = Mathf.Atan2(transform.position.x, transform.position.z);
+        Vector3 pos = transform.position;
+        pos.x = (float)radi*Mathf.Sin(angle);
+        pos.z = (float)radi*Mathf.Cos(angle);
+        transform.position = pos;
     }
 
     void Moving(){
-        Vector3 position;
-        float angle;
-        Vector3 direction, target;
-        float rotationSpeed = 100;
-        position = transform.position;
-        angle = rotationSpeed * Time.deltaTime;
-        direction = position - transform.parent.position;
-        if (Input.GetKey(KeyCode.O))
-        {
-            target = transform.parent.position + Quaternion.AngleAxis(angle, Vector3.up) * direction;
-        }
-        if (Input.GetKey(KeyCode.P))
-        {
-            target = transform.parent.position + Quaternion.AngleAxis(-angle, Vector3.up) * direction;
-        }
-        // Correct orientation of player
-        // Compute current direction
-        Vector3 currentDirection = transform.position - transform.parent.position;
-        currentDirection.y = 0.0f;
-        currentDirection.Normalize();
-        // Change orientation of player accordingly
-        Quaternion orientation;
-        if ((startDirection - currentDirection).magnitude < 1e-3)
-            orientation = Quaternion.AngleAxis(0.0f, Vector3.up);
-        else if ((startDirection + currentDirection).magnitude < 1e-3)
-            orientation = Quaternion.AngleAxis(180.0f, Vector3.up);
-        else
-            orientation = Quaternion.FromToRotation(startDirection, currentDirection);
-        transform.rotation = orientation;
+        Vector2 xz;
+        xz.x = transform.position.x;
+        xz.y = transform.position.z;
+        Debug.Log(transform.position);
+        Debug.Log(transform.parent.position);
+
+        /*xz = xz * 2;
+        Vector3 final = transform.position;
+        final.x += xz.x;
+        final.z += xz.y;
+        transform.position = final;
+        */
     }
 }
